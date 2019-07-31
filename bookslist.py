c7b88c991b65cf12f11b024749a9efc82e34da6f
alleyoop555 <guowei1595@gmail.com>
2019-07-31 16:48:52 +0800

    連線時常有問題

--- bookslist.py
+++ bookslist.py
@@ -1,5 +1,6 @@
 import codecs
 import time
+import json
 
 import pyquery
 import requests
@@ -32,31 +33,56 @@ def getLink(url):
             print('connection is invalid')
             continue
         print('connection is valid')
+        d = pyquery.PyQuery(response.text)
+        posts = d('div.type02_m057:contains("內容簡介")')
         
+        print(posts)
+
         break
+
 
 def getList(url):
     proxyList = proxy.getProxy()
+    rows= []
 
     for proxyItem in proxyList:
         print(f'use proxy {proxyItem["ip"]}:{proxyItem["port"]}')
-        response = connect('銷售榜', url, proxyItem)
+        response = connect('即時榜', url, proxyItem)
         if response is None:
             print('connection is invalid')
             continue
         print('connection is valid')
+        
+        # 開二進位檔案
+        with open('book.b.html', 'wb') as f:
+        # 寫入二進位資料
+            f.write(response.content)
 
         d = pyquery.PyQuery(response.text)
-        posts = d('ul.clearfix div.type02_bd-a')
-        for post in posts.items():
-            title = post('h4')
-            link = title('a').attr('href')
-            print('書名 : ', title.text())
-            print('連結 : ', link)
-            print('----------------------')  
-            getLink(link)   
+        posts = d('ul.clearfix li.item')
 
-        break
+        for idx, post in enumerate(posts.items()):
+            if idx == 0:
+                order = post('strong.no').text()
+                title = post('h4').text()
+                author = post('ul.msg').text()[3:]
+                price = post('li.price_a').text()[4:]
+                link = post('h4 a').attr('href')
+                print('排名 : ', order)
+                print('書名 : ', title)
+                # print('作者 : ', author)
+                # print(price)
+                print('連結 : ', link)
+                print('----------------------')  
+                # getLink(link)   
+                rows.append({
+                    'order':    order, 
+                    'title':    title, 
+                    'link':     link})
+        text = json.dumps(rows, sort_keys=True, indent=4)
+        with codecs.open(f'books.json', 'w', 'utf-8') as f:
+            f.write(text)
+            break
     
     # 開 UTF-8 編碼文字檔案
     # with codecs.open('book.u.html', 'w', encoding='utf-8-sig') as f:
